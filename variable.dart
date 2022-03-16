71303dd76b7d83266430c6b7268bbc09556dedb7
elisha0103 <41459466+elisha0103@users.noreply.github.com>
2022-03-16 16:33:35 +0900

    Create variable.dart

---
+++ variable.dart
@@ -0,0 +1,10 @@
+void main(){
+  //정수 - int, 실수 - double, 참거짓 - bool, 글자 - String
+  //var - 자동맞춤(선언 1회시 타입 고정), dynamic - 자동맞춤(타입 지속변경가능)
+  int number1 = 5;
+  double number2 = 2.5;
+  bool isTrue = true;
+  String name = 'CodeFactory';
+  var name2 = 'redVelvet';
+  dynamic x = 'blackpink';
+}
