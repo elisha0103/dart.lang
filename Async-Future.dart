5ba8d48fbb01f997a6965b71cc8a3df49032ed05
elisha0103 <41459466+elisha0103@users.noreply.github.com>
2022-03-31 22:32:19 +0900

    Update Async-Future.dart

--- Async-Future.dart
+++ Async-Future.dart
@@ -5,13 +5,8 @@ void main(){
   Future<int> number = Future.value(1);
   Future<bool> isTrue = Future.value(true);
   
-  // 2개의 파라미터
-  // delayed - 지연되다.
-  // 1번 파라미터 - 지연할 기간(얼마나 지연할건지) Duration
-  // 2번 파라미터 - 지연 시간이 지난 후 실행할 함수
-  Future.delayed(Duration(seconds : 2), (/*함수매개인자*/){
-    print('Delay 끝');
-  });
+  addNumbers(1,1);
+  addNumbers(2,2);
 }
 
 void addNumbers(int number1 , int number2){
