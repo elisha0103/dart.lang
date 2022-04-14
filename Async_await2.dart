c08a8098028c6af309dd7ce6c2c330bad3eeab3d
elisha0103 <41459466+elisha0103@users.noreply.github.com>
2022-04-14 12:23:38 +0900

    Update Async_await2.dart

--- Async_await2.dart
+++ Async_await2.dart
@@ -31,5 +31,5 @@ Future<int> addNumbers(int number1, int number2) async {
 // await를 적용 안하면 await 코드 다음코드가 실행됨. await 코드 자체만 비동기로 적용되나,
 // await를 적용하면 awiat 코드 다음부터 쭉 비동기로 적용(순서대로)
 // await를 적용한 코드부터 비동기로 적용되고 cpu는 메인함수에서 다음 코드를 수행함(cpu 효율적으로 사용)
-// await의 반환 값은 Future값이 아니다. - 내부는 Future 기능을 적용하지만 결국 로직 순서대로 반환이 되므로 결과값은 Future 값이 아님
+// await의 반환 값은 Future값이 아니다. - 내부는 Future 기능을 적용하지만 결국 로직 순서대로 반환이 됐으므로 Future의 의미를 갖지 않음.
 // Future 반환값과 await 반환 값이 서로 타입이 달라 연산이 안됨. '+'가 안됨.
